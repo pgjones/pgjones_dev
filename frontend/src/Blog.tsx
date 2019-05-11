21a202d38c217c6bead3eb0caee8041992a53a03
pgjones <philip.graham.jones@googlemail.com>
2019-05-11 13:55:45 +0100

    frontend: Add blog history
    This adds links to medium and stet.io blogs I've previously used.

--- frontend/src/Blog.tsx
+++ frontend/src/Blog.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import Card from "./Card";
 import { ColumnContainer } from "./Containers";
 import { SSkeletonLine } from "./Skeleton";
-import { SLink } from "./Typography";
+import { SA, SLink } from "./Typography";
 
 const SH3 = styled.h3`
   margin-bottom: 0;
@@ -53,7 +53,31 @@ const Blog = () => {
       />
     ));
   }
-  return <ColumnContainer>{cards}</ColumnContainer>;
+  return (
+    <ColumnContainer>
+      {cards}
+      <Card
+        header={<SH3>Medium Posts</SH3>}
+        body={
+          <p>
+            Before I made this site I blogged on medium.com under the handle
+            @pgjones. I still do for artciles that are related to my employment.
+          </p>
+        }
+        footer={<SA href="https://medium.com/@pgjones">Medium blog</SA>}
+      />
+      <Card
+        header={<SH3>Stet.io</SH3>}
+        body={
+          <p>
+            Stet.io is an online image editor I develop, it also has a blog
+            about it and image processing techniques.{" "}
+          </p>
+        }
+        footer={<SA href="https://stet.io/blog">Stet.io blog</SA>}
+      />
+    </ColumnContainer>
+  );
 };
 
 export default Blog;
