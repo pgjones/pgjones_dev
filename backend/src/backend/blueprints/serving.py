d6c63c76762112d6450528eb84f4357e1e96f6a1
pgjones <philip.graham.jones@googlemail.com>
2021-05-27 21:39:26 +0100

    backend: Switch to serving the new frontend
    This should cope with the new sveltekit built frontend.

--- backend/src/backend/blueprints/serving.py
+++ backend/src/backend/blueprints/serving.py
@@ -29,30 +29,30 @@ async def index(path: Optional[str] = None) -> ResponseReturnValue:
 
     nonce = token_urlsafe(12)
     try:
-        body = await render_template(f"sapper/{file_name}", nonce=nonce)
+        body = await render_template(f"svelte/{file_name}", nonce=nonce)
     except TemplateNotFound:
-        raise NotFound()
-    else:
-        response = await make_response(body)
-        response.headers["Content-Security-Policy"] = ""
-        response.content_security_policy.default_src = "'self'"
-        response.content_security_policy.base_uri = "'self'"
-        response.content_security_policy.form_action = "'self'"
-        response.content_security_policy.frame_ancestors = "'none'"
-        response.content_security_policy.frame_src = "https://www.youtube-nocookie.com"
-        response.content_security_policy.img_src = "'self' data:"
-        response.content_security_policy.script_src = f"'self' 'nonce-{nonce}'"
-        response.content_security_policy.style_src = f"'self' 'nonce-{nonce}'"
+        body = await render_template(f"svelte/fallback.html", nonce=nonce)
 
-        response.headers["Referrer-Policy"] = "no-referrer, strict-origin-when-cross-origin"
-        response.headers["X-Frame-Options"] = "SAMEORIGIN"
-        response.headers["X-XSS-Protection"] = "1; mode=block"
-        return response
+    response = await make_response(body)
+    response.headers["Content-Security-Policy"] = ""
+    response.content_security_policy.default_src = "'self'"
+    response.content_security_policy.base_uri = "'self'"
+    response.content_security_policy.form_action = "'self'"
+    response.content_security_policy.frame_ancestors = "'none'"
+    response.content_security_policy.frame_src = "https://www.youtube-nocookie.com"
+    response.content_security_policy.img_src = "'self' data:"
+    response.content_security_policy.script_src = f"'self' 'nonce-{nonce}'"
+    response.content_security_policy.style_src = f"'self' 'nonce-{nonce}'"
+
+    response.headers["Referrer-Policy"] = "no-referrer, strict-origin-when-cross-origin"
+    response.headers["X-Frame-Options"] = "SAMEORIGIN"
+    response.headers["X-XSS-Protection"] = "1; mode=block"
+    return response
 
 
-@blueprint.route("/client/<path:path>")
+@blueprint.route("/_app/<path:path>")
 async def client_static(path: str) -> ResponseReturnValue:
-    return await current_app.send_static_file(f"sapper/{path}")
+    return await current_app.send_static_file(f"_app/{path}")
 
 
 @blueprint.route("/blog/atom.xml")
@@ -63,12 +63,6 @@ async def atom_feed() -> ResponseReturnValue:
 @blueprint.route("/blog/rss20.xml")
 async def rss_feed() -> ResponseReturnValue:
     return current_app.feeds[0], {"Content-Type": "text/xml"}
-
-
-@blueprint.route("/service-worker.js")
-async def service_worker() -> ResponseReturnValue:
-    path = safe_join(current_app.static_folder, "js", "service-worker.js")
-    return await send_file(path)
 
 
 @blueprint.route("/manifest.json")
