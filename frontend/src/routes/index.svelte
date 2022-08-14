d4b7a3d15792c59625a92bff2f9f83ae0203e39d
pgjones <philip.graham.jones@googlemail.com>
2022-08-14 17:12:57 +0100

    frontend: Update my open source project list

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -30,8 +30,8 @@
       <Project
         name="Quart"
         imgSrc="/static/media/Quart"
-        sourceHref="https://gitlab.com/pgjones/quart"
-        docsHref="https://pgjones.gitlab.io/quart/"
+        sourceHref="https://gitlab.com/pallets/quart"
+        docsHref="https://quart.palletsprojects.com"
       >
         Quart is a Python ASGI web microframework, that allows websites to be
         written utilising the async await syntax and the familiar Flask API.
@@ -52,7 +52,8 @@
 
       <Project
         name="Quart-Trio"
-        sourceHref="https://gitlab.com/pgjones/quart-trio"
+        sourceHref="https://github.com/pgjones/quart-trio"
+        docsHref="https://quart-trio.readthedocs.io/"
       >
         Quart-Trio is a Quart extension that supports the
         <a href="https://trio.readthedocs.io/en/stable/">Trio</a>
@@ -62,7 +63,7 @@
 
       <Project
         name="Quart-Cors"
-        sourceHref="https://gitlab.com/pgjones/quart-cors"
+        sourceHref="https://github.com/pgjones/quart-cors"
       >
         Quart-CORS is a Quart extension that adds relevant Cross Origin Resource
         Sharing headers.
@@ -70,7 +71,7 @@
 
       <Project
         name="Quart-Rate-Limiter"
-        sourceHref="https://gitlab.com/pgjones/quart-rate-limiter"
+        sourceHref="https://github.com/pgjones/quart-rate-limiter"
       >
         Quart-Rate-Limiter is an extension for Quart to allow for rate limits to
         be defined and enforced on a per route basis. It is interesting in that
@@ -79,8 +80,8 @@
 
       <Project
         name="Quart-Auth"
-        sourceHref="https://gitlab.com/pgjones/quart-auth"
-        docsHref="https://pgjones.gitlab.io/quart-auth/"
+        sourceHref="https://github.com/pgjones/quart-auth"
+        docsHref="https://quart-auth.readthedocs.io/"
       >
         Quart-Auth is an extension for Quart to provide for secure cookie
         authentication (session management). It allows for a session to be
@@ -89,12 +90,21 @@
 
       <Project
         name="Quart-Schema"
-        sourceHref="https://gitlab.com/pgjones/quart-schema"
-        docsHref="https://pgjones.gitlab.io/quart-schema/"
+        sourceHref="https://github.com/pgjones/quart-schema"
+        docsHref="https://quart-schema.readthedocs.io/"
       >
         Quart-Schema is a Quart extension that provides schema validation and
         auto-generated API documentation. This is particularly useful when
         writing RESTful APIs.
+      </Project>
+
+      <Project
+        name="Quart-DB"
+        sourceHref="https://github.com/pgjones/quart-db"
+        docsHref="https://quart-db.readthedocs.io/en/latest/"
+      >
+        Quart-DB is a Quart extension that provides managed connection(s) to
+        postgresql database(s).
       </Project>
 
       <Project
@@ -117,10 +127,19 @@
       <Project
         name="Werkzeug"
         sourceHref="https://github.com/pallets/werkzeug"
+        docsHref="https://werkzeug.palletsprojects.com"
         role="MAINTAINER"
       >
-        Werkzeug is a comprehensive WSGI web application library, my effort is
-        to make it a ASGI web application library as well.
+        Werkzeug is a comprehensive WSGI web application library.
+      </Project>
+
+      <Project
+        name="Flask"
+        sourceHref="https://github.com/pallets/flask"
+        docsHref="https://flask.palletsprojects.com"
+        role="MAINTAINER"
+      >
+        Flask is a lightweight WSGI web application framework.
       </Project>
     </div>
   </div>
