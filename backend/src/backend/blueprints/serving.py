pgjones/pgjones_dev

commit 5601863bf63a91c1f0234e602abe6bf9b07ca51c

Remove the old book and replace with information about the new one
I've completed the concept of the tozo book, but in a complete form
that has been edited and published. This switches to that book.

backend/src/backend/blueprints/serving.py
@@ -67,18 +67,6 @@ async def client_static(path: str) -> ResponseReturnValue:
     return await current_app.send_static_file(f"_app/{path}")
 
 
-@blueprint.route("/tozo/", defaults={"path": ""})
-@blueprint.route("/tozo/<path:path>")
-async def tozo(path: str) -> ResponseReturnValue:
-    try:
-        response = await send_from_directory(current_app.root_path, f"tozo/{path}")
-    except NotFound:
-        response = await send_from_directory(
-            current_app.root_path, f"tozo/{path}/index.html"
-        )
-    return _apply_security_headers(response)
-
-
 @blueprint.route("/blog/atom.xml")
 async def atom_feed() -> ResponseReturnValue:
     return current_app.feeds[1], {"Content-Type": "text/xml"}  # type: ignore

commit 3156ccb6c8bff8a8e0c330697702d9f964c747da

backend: Fix linting
Error introduced in 5601863bf63a91c1f0234e602abe6bf9b07ca51c

backend/src/backend/blueprints/serving.py
@@ -9,10 +9,8 @@ from quart import (
     current_app,
     make_response,
     render_template,
-    send_from_directory,
 )
 from quart.helpers import safe_join, send_file
-from werkzeug.exceptions import NotFound
 from werkzeug.http import COOP
 from werkzeug.sansio.response import Response
 